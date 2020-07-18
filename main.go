d325eecbe219f7bca067c46c563e58cb541909b5
Henrique Alves Junkes <henriquejunkes887@gmail.com>
2020-07-18 00:48:36 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package goarea
+
+import "math"
+
+// Pi é uma proporção numérica definida pela relação entre o perímetro de uma circunferência e seu diâmetro
+const Pi = 3.1416
+
+// Circ é responsavel por calcular a área da circunferência
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+}
+
+// React é responsavel por calcular a área de um retangulo
+func React(base, altura float64) float64 {
+	return base * altura
+}
+
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
+}
